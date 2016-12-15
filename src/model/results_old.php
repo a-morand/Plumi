7861255557945c470b3cc71c229d77d69e47bdc1
Axel <axel.morand@hetic.net>
2016-12-15 19:28:54 +0100

    Result page done (display questions 1,2,3)

--- src/model/results_old.php
+++ src/model/results_old.php
@@ -16,13 +16,12 @@ class Results
 
     public function getFill($id)
     {   
-            $sql = 'SELECT a.* FROM answers AS a JOIN fills AS f ON (a.fill_id = f.id) WHERE f.user_id = :user';
+            $sql = 'SELECT * FROM answers WHERE fill_id = :fill';
             $stmt = $this->db->prepare($sql);
-            $stmt->bindValue('user', $id);
+            $stmt->bindValue('fill', $id);
             $stmt->execute();
 
             $data = $stmt->fetchAll();
-
             foreach ($data as &$row) {
                 $type_id = $row->type_id;
                 $question_id = $row->question_id;
@@ -35,6 +34,7 @@ class Results
                 $row->question = $stmt->fetch();
 
             }
+
             return $data;
     }
 }
